fd17f8757cc709122aee811a22e8b00b4aa0acde
Diego Gonzaga <diego-gonzaga@live.com>
2022-06-05 13:12:08 -0300

    Login with state

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,29 @@
 import { Button, Input, Stack } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-    return <Stack spacing={4} >
-        <Input placeholder='Username' />
-        <Input type="password" placeholder='Password' />
-        <Button colorScheme='teal' >Log In</Button>
-    </Stack>
-}
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+
+    const login = () => {
+        console.log(`${username} + ${password}`);
+    };
+    return (
+        <Stack spacing={4}>
+            <Input
+                placeholder="Username"
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <Input
+                type="password"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button colorScheme="teal" onClick={login}>
+                Log In
+            </Button>
+        </Stack>
+    );
+};
 
 export default Login;
